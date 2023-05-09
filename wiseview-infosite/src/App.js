941164f193ea8b71a78df0871590208f22f64f8d
Atud Jerry <atudjerry33@gmail.com>
2023-05-09 10:46:24 +0100

    top bar components edited

--- wiseview-infosite/src/App.js
+++ wiseview-infosite/src/App.js
@@ -1,11 +1,13 @@
+import Topbar from "./Components/Topbar"
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+           <Topbar/>
       <header className="App-header">
-      
+   
       
        
       
--- wiseview-infosite/src/Components/Topbar.js
+++ wiseview-infosite/src/Components/Topbar.js
@@ -1,5 +1,55 @@
 import react from 'react'
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import { IconButton } from '@mui/material';
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Topbar = () => {
+
+
+    return (
+        <div className='topbar'>
+       <div className='menubar'>  <IconButton >   <MenuOutlinedIcon color ="secondary" fontSize='large'/> </IconButton> </div>
+       <div>
+        
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+        <h2>MEMBERS</h2>
+        <ul type = "none">
+            <li>Steve Yonkeu</li>
+            <li>Atud Jerry</li>
+            <li>Ndongmo Christian</li>
+            <li>Fotsingboris</li>
+            <li>Manka Velda</li>
+
+
+
+
+        </ul>
+
+
+
+
+       </div>
+
+
+        </div>
+    )
+
     
 }
+export default Topbar
